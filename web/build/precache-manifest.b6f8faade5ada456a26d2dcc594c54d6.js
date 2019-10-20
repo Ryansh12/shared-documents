e9c0c4d25527be141c99c2f8b100411057ac5e3a
Ryansh12 <pratikmane239@gmail.com>
2019-10-20 20:44:53 +0530

    Initial commit

---
+++ web/build/precache-manifest.b6f8faade5ada456a26d2dcc594c54d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd6d7b004fa18f0412c5e37b5725aa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d9f0a5e5d47634b79b",
+    "url": "/static/js/2.4de96018.chunk.js"
+  },
+  {
+    "revision": "cd3c29e73fbb55d52a75",
+    "url": "/static/js/main.ca7ee083.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
